cfcaa29eef8d485d53be2a71b15d6f18de1daf7a
Chirag Raninga <chirag.raninga@aitrade.ai>
2021-08-11 23:06:53 +0530

    PT-896 Remove prefix from reference table

--- src/Command/GenerateEntitiesCommand.php
+++ src/Command/GenerateEntitiesCommand.php
@@ -25,7 +25,8 @@ class GenerateEntitiesCommand extends Command
             ->addOption('namespace', null, InputOption::VALUE_REQUIRED, 'Namespace', 'App\\Entity')
             ->addOption('directory', null, InputOption::VALUE_REQUIRED, 'Output directory', 'src\\Entity')
             ->addOption('collection-implementation', null, InputOption::VALUE_REQUIRED, 'Collection implementation', '\\Doctrine\\Common\\Collections\\ArrayCollection')
-            ->addOption('collection-interface', null, InputOption::VALUE_REQUIRED, 'Collection interface', '\\Doctrine\\Common\\Collections\\Collection');
+            ->addOption('collection-interface', null, InputOption::VALUE_REQUIRED, 'Collection interface', '\\Doctrine\\Common\\Collections\\Collection')
+            ->addOption('prefix',null, InputOption::VALUE_OPTIONAL, 'Remove prefix from reference table');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output): ?int
@@ -35,6 +36,7 @@ class GenerateEntitiesCommand extends Command
         $collectionImplementation = $input->getOption('collection-implementation');
         $collectionInterface = $input->getOption('collection-interface');
         $outputDirection = $input->getOption('directory');
+        $prefix = $input->getOption('prefix');
 
         if (!$dsnInput) {
             throw new \InvalidArgumentException('A DSN is required');
@@ -53,8 +55,11 @@ class GenerateEntitiesCommand extends Command
 
         /** @var Entity $entity */
         foreach ($generator->generateEntities() as $entity) {
-            if(strpos($entity->getTable(), 'migration_versions')) {
+            if (strpos($entity->getTable(), 'migration_versions')) {
                 continue;
+            }
+            if(!empty($prefix)) {
+                $this->removePrefixFromReference($entity, $prefix);
             }
             $fileName = $outputDirection . '/' . Namer::entityName($entity->getTable()) . '.php';
             $class = $renderer->render($entity, $namespace, $collectionInterface, $collectionImplementation);
@@ -62,5 +67,16 @@ class GenerateEntitiesCommand extends Command
         }
 
         return 0;
+    }
+
+    private function removePrefixFromReference(Entity $entity, string $prefix)
+    {
+        if(!empty($references = $entity->getReferences())) {
+            foreach ($references as $reference) {
+                if(!$reference->isOwningSide()) {
+                    $table = str_replace($prefix.'_','',$reference->getTable());
+                }
+            }
+        }
     }
 }
